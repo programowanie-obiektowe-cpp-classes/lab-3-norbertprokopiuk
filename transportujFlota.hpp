ddefb093cd25e95b4085a4d8b456344b0705b2da
norbertprokopiuk <61018774+norbertprokopiuk@users.noreply.github.com>
2022-11-16 19:47:56 +0100

    zaliczenie

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,22 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    Stocznia     stocznia{};
+    unsigned int liczba_zaglowcow = 0;
+    unsigned int dotychczasowy_towar=0;
+    if (towar == 0) {
+        return 0;
+    }
+    else {
+        while (dotychczasowy_towar <= towar) {
+            Statek* s = stocznia(); // stworz statek
+            dotychczasowy_towar += s->transportuj();
+
+            if(dynamic_cast<Zaglowiec *>(s) != 0)
+            {
+            liczba_zaglowcow++;
+            }
+        }
+        return liczba_zaglowcow;
+    }
 }
